2e3a4552c521cce2930fe31cd116c0b28c7e371a
sagarshah1729 <sagarshah2326@gmail.com>
2016-08-28 16:21:29 +0000

    Lettuce: Remove Sepolicy for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,9 +64,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-BOARD_SEPOLICY_DIRS += \
-    device/yu/lettuce/sepolicy
-
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Tap to wake
